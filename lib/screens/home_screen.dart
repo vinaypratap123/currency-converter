f95f2e86984241a1ca1024c3d29f592c442a8335
vinay pratap <vinaypratapknj12@gmail.com>
2023-12-18 18:56:48 +0530

    set up project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Currency Converter")),
+      body: const Center(child: Text("currency converter")),
+    );
+  }
+}
